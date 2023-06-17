c22f94e1a0a43f75d2c2a8efc4daa894159491ce
mig <michaelleojacob@gmail.com>
2023-06-17 16:34:29 -0700

    basic routing is set up.
    *added z-index to app to ensure the particles are in the background

---
+++ src/components/testComponents/testComponents.tsx
@@ -0,0 +1,23 @@
+const Home = () => {
+  return (
+    <>
+      <div className="text-white text-6xl">Home</div>
+    </>
+  );
+};
+const About = () => {
+  return (
+    <>
+      <div className="text-white text-6xl">About</div>
+    </>
+  );
+};
+const Contact = () => {
+  return (
+    <>
+      <div className="text-white text-6xl">Contact</div>
+    </>
+  );
+};
+
+export { Home, About, Contact };
