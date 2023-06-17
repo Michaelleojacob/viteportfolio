02f831dd9f05608d10a2443f0293898c24ae28ab
mig <michaelleojacob@gmail.com>
2023-06-17 15:57:15 -0700

    +added and commented out the particle debug logs.

--- src/components/particles/particles.tsx
+++ src/components/particles/particles.tsx
@@ -6,11 +6,13 @@ import options from "./options";
 
 const ParticleAnimation = () => {
   const particlesInit = useCallback(async (engine: Engine) => {
+    // console.log(engine);
     await loadFull(engine);
   }, []);
 
   const particlesLoaded = useCallback(
     async (container: Container | undefined) => {
+      // await console.log(container);
       await container;
     },
     []
