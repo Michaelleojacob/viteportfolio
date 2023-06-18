43a4788a4858d63a79065fb36cd2e3707e69d157
mig <michaelleojacob@gmail.com>
2023-06-17 17:52:32 -0700

    +navbar renders burger or all options based on screen size

---
+++ src/components/nav/smScreen.tsx
@@ -0,0 +1,14 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import Button from "@mui/material/Button";
+
+const NavSmScreen = () => {
+  return (
+    <div>
+      <Button>
+        <MenuIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default NavSmScreen;
