7d7a58be01ea406b7fe3076cb6742dd799d1afed
mig <michaelleojacob@gmail.com>
2023-06-18 20:19:00 -0700

    uncommented lgAppCard info

--- src/components/apps/cards/largeAppCard.tsx
+++ src/components/apps/cards/largeAppCard.tsx
@@ -11,14 +11,14 @@ const LgAppCard: React.FC<{ app: App }> = ({ app }) => {
         height={300}
         width={300}
       />
-      {/* <a href={app.demo}>{app.title}</a>
+      <a href={app.demo}>{app.title}</a>
       <div className="flex">
         <IconLinks linkurl={app.repo} icontype="github" />
         <IconLinks linkurl={app.demo} icontype="external" />
       </div>
       <div className="flex">
         <AppIcons icons={app.icons} />
-      </div> */}
+      </div>
     </div>
   );
 };
